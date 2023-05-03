8af464c1270579cc2ef923d90276ed979cd444e7
tiennt171 <tiennt171@ghtk.co>
2023-05-03 21:58:32 +0700

    Learn PHP

---
+++ c1-php-basic/c1.php
@@ -0,0 +1,11 @@
+<?PHP
+
+/**
+ * - Tên hàm và cấu trúc ngôn ngữ PHP không phân biệt chữ hoa chữ thường, nhưng tên biến và tên hằng số thì có.
+ *
+ */
+
+
+ECHO "Hello World"; // hoạt động
+$variable = "Hello World";
+echo $VARIABLE; // lỗi
