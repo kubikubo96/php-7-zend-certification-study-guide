91406214563f8e66e996637caf7e20fe2e30cbcb
tiennt171 <tiennt171@ghtk.co>
2023-05-03 21:58:05 +0700

    Learn PHP

---
+++ c1-php-basic/c2.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * - Câu lệnh echo được viết cuối cùng trước thẻ đóng, do đó không cần dấu chấm phẩy để kết thúc nó.
+ * ( đây là trường hợp đặc biệt không cần kết thúc bằng dấu chấm phẩy (;) trong PHP )
+ */
+
+
+$variable = "Hello World";
+echo "Hello World"
+?>
