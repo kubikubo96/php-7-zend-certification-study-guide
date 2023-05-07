af28f52865bf763b20cf9bfc315b3a71085e36b1
tiennt171 <tiennt171@ghtk.co>
2023-05-07 16:39:45 +0700

    Learn PHP

--- c1-php-basic/c2.php
+++ c1-php-basic/c2.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
- * - Câu lệnh echo được viết cuối cùng trước thẻ đóng, do đó không cần dấu chấm phẩy để kết thúc nó.
- * ( đây là trường hợp đặc biệt không cần kết thúc bằng dấu chấm phẩy (;) trong PHP )
+ * Tất cả các câu lệnh trong PHP phải được kết thúc bằng dấu chấm phẩy. Một ngoại lệ cho điều này là nếu câu
+ * lệnh xảy ra là câu lệnh cuối cùng trước thẻ đóng.
  */
 
 
